56ccfbaf54295acd387bc7fe4a0794d3ce64b8d7
AzureDream <ducphamtan2098@gmail.com>
2025-11-30 15:51:41 +0700

    setup apps for django

--- Django_Python_Project/config/settings.py
+++ Django_Python_Project/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
+    'apps.products',
+    'apps.cart',
+    'apps.orders',
+    'apps.reviews',
+    'apps.catalog'
 ]
 
 MIDDLEWARE = [
