dfb978412d58b77324015f0c95a4bc3f4f5090ec
AzureDream <ducphamtan2098@gmail.com>
2025-12-23 00:30:40 +0700

    refactor(order, product): slight change to add css

--- Django_Python_Project/apps/orders/urls.py
+++ Django_Python_Project/apps/orders/urls.py
@@ -8,7 +8,7 @@ app_name = 'orders'
 
 urlpatterns = [
     # path('create/', views.order_create, name='order_create'),
-    # path('list/', views.order_list, name='order_list'),
-    # path('<int:order_id>/', views.order_detail, name='order_detail'),
+    path('list/', views.order_list, name='order_list'),
+    path('<int:order_id>/', views.order_detail, name='order_detail'),
 ]
 
--- Django_Python_Project/apps/orders/views.py
+++ Django_Python_Project/apps/orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def order_create(request):
+    pass
+
+def checkout(request):
+    return render(request, "Django_Python_Project\\templates\\orders\\checkout.html")
+
+def order_detail(request):
+    return render(request, "Django_Python_Project\\templates\\orders\\order_detail.html")
+
+def order_list(request):
+    return render(request, "Django_Python_Project\\templates\\orders\\orderlist.html")
